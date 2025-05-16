9c981172db5661e81bbc287267d24d7372fc316e
marco-rubio-bustos <marco.rubio.bustos@gmail.com>
2025-05-15 22:02:39 -0400

    ordena los types

--- src/types/quotation.tsx
+++ src/types/quotation.tsx
@@ -1,16 +1,5 @@
-export interface Quotation {
-  id: string
-  name: string
-  address: string
-  rut: string
-  attention: string
-  email: string
-  phone: string
-  idPrice: string
-  subTotal: string
-  iva: string
-  total: string
-  description: string
-  notesGeneral: string
+import { TypeBase } from './typeBase'
+
+export interface Quotation extends TypeBase {
   created_at: string
 }
